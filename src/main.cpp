445176a05f0228d377f18bb6b6bae5a88ee7d69b
minkyo <minkyo.kim@5minlab.com>
2026-01-01 23:45:53 +0900

    window mac 크로스 플랫폼 다시

--- src/main.cpp
+++ src/main.cpp
@@ -6,62 +6,57 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-
-#include "../include/navi/geometry/CubeGeometry.h"
-#include "../include/navi/gfx/Mesh.h"
-
-void framebuffer_size_callback(GLFWwindow*window , int width, int height)
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
 
-int main() {
-        // ---- GLFW init ----
-        if (!glfwInit()) {
-            throw std::runtime_error("glfwInit() failed");
-        }
-
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    #ifdef __APPLE__
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    #endif
-
-        GLFWwindow* window = glfwCreateWindow(1280, 720, "my_navi - cube", nullptr, nullptr);
-        if (window == nullptr) {
-            glfwTerminate();
-            throw std::runtime_error("glfwCreateWindow() failed");
-        }
-
-        glfwMakeContextCurrent(window); // 만든 창 context로 지정.
-       
-        // ---- GLAD load ----
-        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-            glfwDestroyWindow(window);
-            glfwTerminate();
-            throw std::runtime_error("gladLoadGLLoader() failed");
-        }
-
-        glViewport(0,0,1280,720);
-        glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
-
-
-        
-    while(!glfwWindowShouldClose(window))
+int main()
+{
+    // ---- GLFW init ----
+    if (!glfwInit())
     {
-        //render
+        throw std::runtime_error("glfwInit() failed");
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "my_navi - cube", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        glfwTerminate();
+        throw std::runtime_error("glfwCreateWindow() failed");
+    }
+
+    glfwMakeContextCurrent(window); // 만든 창 context로 지정.
+
+    // ---- GLAD load ----
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        throw std::runtime_error("gladLoadGLLoader() failed");
+    }
+
+    glViewport(0, 0, 1280, 720);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // render
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // 상태 함수
-        glClear(GL_COLOR_BUFFER_BIT); // 위에서 상태 설정하고 여기서 지움.
+        glClear(GL_COLOR_BUFFER_BIT);         // 위에서 상태 설정하고 여기서 지움.
 
         glfwSwapBuffers(window);
-        glfwPollEvents();    
-        
+        glfwPollEvents();
     }
 
     glfwTerminate();
-    
- return 0;
+
+    return 0;
 }
